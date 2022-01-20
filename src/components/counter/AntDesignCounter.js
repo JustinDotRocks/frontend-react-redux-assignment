d1a761b2ee1a0ee8ebac7a7a84b5e8589152aca9
Justin <justins192107@gmail.com>
2022-01-20 16:34:22 -0330

    added the card from antd using styled-component called StyledCard. gave the card a width, margin-top, font-size and a border.

--- src/components/counter/AntDesignCounter.js
+++ src/components/counter/AntDesignCounter.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Card } from 'antd';
+
+const StyledCard = styled(Card)`
+	width: 15em;
+	margin-top: 2em;
+	font-size: 0.75em;
+	border: 2px solid #e9fff9;
+`;
 
 class AntDesignCounter extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<div>
+				<StyledCard title="Ant Design Counter">
+					<p>StyledCounter</p>
+					<p>ResetButton</p>
+					<p> counterHistory</p>
+				</StyledCard>
+			</div>
+		);
 	}
 }
 
