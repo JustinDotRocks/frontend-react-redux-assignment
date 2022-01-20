0d5eba775c3317cf20f416e14dc1e7d92795308f
Justin <justins192107@gmail.com>
2022-01-20 16:57:22 -0330

    added the StyledCounter to the StyledCard and gave it some style.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './components/counter/Counter';
 import './App.css';
 import AntDesignCounter from './components/counter/AntDesignCounter';
--- src/components/counter/AntDesignCounter.js
+++ src/components/counter/AntDesignCounter.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectCount } from './counterSlice';
 import styled from 'styled-components';
 import { Card } from 'antd';
 
@@ -7,20 +9,38 @@ const StyledCard = styled(Card)`
 	margin-top: 2em;
 	font-size: 0.75em;
 	border: 2px solid #e9fff9;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
-class AntDesignCounter extends Component {
-	render() {
-		return (
-			<div>
-				<StyledCard title="Ant Design Counter">
-					<p>StyledCounter</p>
-					<p>ResetButton</p>
-					<p> counterHistory</p>
-				</StyledCard>
-			</div>
-		);
-	}
-}
+const StyledCounter = styled.div`
+	border: 2px solid #e9fff9;
+	border-radius: 5px;
+	width: 5em;
+	height: 2em;
+	margin-top: 1em;
+	font-size: 1.5em;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: rebeccapurple;
+	font-weight: bold;
+	background-color: #111111;
+`;
+
+const AntDesignCounter = () => {
+	const count = useSelector(selectCount);
+
+	return (
+		<div>
+			<StyledCard title="Ant Design Counter">
+				<StyledCounter>{count}</StyledCounter>
+				<p>ResetButton</p>
+				<p> counterHistory</p>
+			</StyledCard>
+		</div>
+	);
+};
 
 export default AntDesignCounter;
